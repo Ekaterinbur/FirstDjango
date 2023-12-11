3890e9d9dd01103c9070de9537aed3896c878fb7
Ekaterinbur <kate-buravkova@mail.ru>
2023-12-11 23:50:37 +0300

    Done. M1:task1.1

--- FirstDjango/urls.py
+++ FirstDjango/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from MainApp import views
 
 urlpatterns = [
-    path('', views.home),
+    path('', views.home ),
+    path("about", views.about),
 ]
--- MainApp/views.py
+++ MainApp/views.py
@@ -3,9 +3,26 @@ from django.http import HttpResponse
 
 # Create your views here.
 
+author = {
+    "Имя": "Иван",
+    "Фамилия": "Иванов"
+}
+
 def home(request):
     text = """
     <h1> "Изучаем django" </h1>
     <strong> Автор </strong>:  <i> Буравкова Е.И. </i>
+    """
+    return HttpResponse (text)
+
+
+def about(request):
+    text = f"""
+
+
+    Имя: <b> {author ["Имя"]} </b> <br>
+    Фамилия: <b> {author ["Фамилия"]} </b> <br>
+
+    
     """
     return HttpResponse (text)
