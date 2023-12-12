d73ca139bbe8bc95fb464c716d324fe2b677bb62
Ekaterinbur <kate-buravkova@mail.ru>
2023-12-12 22:16:36 +0300

    DZ1_all_task

--- MainApp/views.py
+++ MainApp/views.py
@@ -1,12 +1,23 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseNotFound
 
 # Create your views here.
 
 author = {
     "Имя": "Иван",
-    "Фамилия": "Иванов"
-}
+    "Отчество": "Петрович",
+    "Фамилия": "Иванов",
+    "телефон": "8-923-600-01-02",
+    "email": "vasya@mail.ru"
+    }  
+
+items = [
+    {"id": 1, "name": "Кроссовки abibas" ,"quantity":5},
+    {"id": 2, "name": "Куртка кожаная" ,"quantity":2},
+    {"id": 5, "name": "Coca-cola 1 литр" ,"quantity":12},
+    {"id": 7, "name": "Картофель фри" ,"quantity":0},
+    {"id": 8, "name": "Кепка" ,"quantity":124},
+    ]
 
 def home(request):
     text = """
@@ -15,14 +26,30 @@ def home(request):
     """
     return HttpResponse (text)
 
-
 def about(request):
     text = f"""
-
-
-    Имя: <b> {author ["Имя"]} </b> <br>
-    Фамилия: <b> {author ["Фамилия"]} </b> <br>
-
-    
+    Имя: <b>{author["Имя"]}</b><br>
+    Отчество: <b>{author["Отчество"]}</b><br>
+    Фамилия: <b>{author["Фамилия"]}</b><br>
+    телефон: <b>{author['телефон']}</b><br>
+    email: <b>{author['email']}</b><br>
     """
     return HttpResponse (text)
+
+def get_item(request,item_id: int):
+    for item in items: 
+        if item["id"] == item_id :
+            result = f"""
+            <h2> Имя : {item["name"]} </h2> 
+            <p> количество : {item["quantity"]} </p> 
+            <p> <a href="/items"> Назад к списку товаров </a></p>
+            """
+            return HttpResponse (result)
+    return HttpResponseNotFound (f'Item with id = {item_id} not found.')
+
+def get_items(request):
+    result = f"<h2> Cписок товаров </h2> <ol>"
+    for item in items:
+        result += f""" <li><a href="/item/{item ["id"]}"> {item ["name"]} </a></li>"""
+    result += "</ol>"
+    return HttpResponse (result)
